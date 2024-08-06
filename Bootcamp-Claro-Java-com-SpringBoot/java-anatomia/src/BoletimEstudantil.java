b82add0127bb0b9f5eb87812bf2c979473e3e8d0
FeehOliveira <feeholiveira10@outlook.com>
2024-08-06 01:59:07 -0300

    finalizando-modulo

---
+++ Bootcamp-Claro-Java-com-SpringBoot/java-anatomia/src/BoletimEstudantil.java
@@ -0,0 +1,10 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        
+        int mediaFinal = 6;
+        if (mediaFinal <=4)
+            System.out.println("Reprovado");
+            else if (mediaFinal >= 6)    
+            System.out.println("Aprovado");
+    }
+}
